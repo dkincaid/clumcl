76ac80b94a9f5e6c0ec23ffd1c69ca2496290e9f
Dave Kincaid <kincaid.dave@gmail.com>
2014-02-12 10:43:33 -0600

    Initial project template

---
+++ README.md
@@ -0,0 +1,14 @@
+# clumcl
+
+A Clojure UMLS client library
+
+## Usage
+
+Coming soon.
+
+## License
+
+Copyright © 2014 Dave Kincaid
+
+Distributed under the Eclipse Public License either version 1.0.
+
---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject clumcl "0.1.0-SNAPSHOT"
+  :description "The Clojure UMLS Client Library"
+  :url "http://github.com/dkincaid/clumcl"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/java" "src/generated/java"]
+  :test-paths ["test/clj" "test/java"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.taoensso/timbre "3.0.0"]]
+  :profiles {:dev {:dependencies [[midje "1.6.0"]]}})
