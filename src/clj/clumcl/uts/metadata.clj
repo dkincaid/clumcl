396b70853b745def0e632a281141aa1f881f07f9
Dave Kincaid <kincaid.dave@gmail.com>
2014-02-16 19:41:53 -0600

    Finishing up the metadata API calls

--- src/clj/clumcl/uts/metadata.clj
+++ src/clj/clumcl/uts/metadata.clj
@@ -1,6 +1,7 @@
 (ns clumcl.uts.metadata
   "This namespace contains functions for interacting with the UMLS meta thesaurus web service API for metadata."
   (:require [clumcl.uts.security :as sec]
+            [camel-snake-kebab :refer [->kebab-case]]
             [taoensso.timbre :as timbre
              :refer (trace debug info warn error fatal spy with-log-level)])
   (:import [gov.nih.nlm.uts.webservice.metadata UtsWsMetadataControllerImplService]))
@@ -8,55 +9,74 @@
 (def metadata-service
   (.getUtsWsMetadataControllerImplPort (UtsWsMetadataControllerImplService.)))
 
-(defmacro defmetadatafn
-  "Creates a function that calls the given metadata API method. It adds the conn 
-argument first then all the provided arguments for the function. If
-a transform-fns are provided it will thread the result of the method call 
-through apply them using the ->> threading macro.
+(defmacro gen-fn
+  [meth args transform-fns]
+  `(fn [~'conn ~@args]
+     (->> (. metadata-service
+             ~meth
+             (sec/single-use-ticket ~'conn)
+             ~@args)
+          ~@transform-fns)))
 
-For example:
+(defmacro def-fns [& args]
+  `(do ~@(for [[meth args transform-fns] (partition 3 args)
+               :let [name (symbol (->kebab-case (str meth)))]]
+           `(def ~name (gen-fn ~meth ~args ~transform-fns)))))
 
-   (defmetadatafn get-current-umls-version
-     \"Get the version\"
-     []
-     getCurrentUMLSVersion)
+(def-fns
+  ;;; UMLS Version Metadata
+  getCurrentUMLSVersion [] [identity]
+  getAllUMLSVersions [] [identity]
 
-will generate a function with the following signature:
+  ;;; Source Metadata
+  getRootSource [version root-source] [bean]
+  getAllRootSources [version] [(map bean)]
+  getAllRootSourceFamilies [version] [(map bean)]
+  getRootSourcesByFamily [version family] [(map bean)]
+  getRootSourcesByRestrictionLevel [version restriction-level] [(map bean)]
+  getVersionedSources [version root-source-abbrev] [(map bean)]
+  getCurrentVersionSource [version root-source-abbrev] [bean]
+  getUpdatedSourcesByVersion [version] [(map bean)]
+  getSource [version source] [bean]
+  getAllSources [version] [(map bean)]
 
-   (defn get-current-umls-version
-     \"Get the version\"
-     [conn]
-     ...)"
-  [f docstring args api-name & transform-fns]
-  (if transform-fns
-      `(defn ~f ~docstring [~'conn ~@args]
-         (->> (. metadata-service ~api-name (sec/single-use-ticket ~'conn) ~@args)
-              ~@transform-fns))
-      `(defn ~f ~docstring [~'conn ~@args]
-         (. metadata-service ~api-name (sec/single-use-ticket ~'conn) ~@args))))
+  ;;; Identifier Metadata
+  getIdentifierType [version identifier] [bean]
 
-(defmetadatafn get-current-umls-version
-  "Get the current UMLS version"
-  []
-  getCurrentUMLSVersion)
+  ;;; Language Metadata
+  getLanguage [version language] [bean]
+  getAllLanguages [version] [(map bean)]
+  getRootSourcesByLanguage [version language] [(map bean)]
 
-(defmetadatafn get-all-umls-versions
-  "Get all the UMLS versions."
-  []
-  getAllUMLSVersions)
+  ;;; Source citation metadata
+  getAllSourceCitations [version] [(map bean)]
+  getSourceCitation [version handle] [bean]
 
-(defmetadatafn get-all-sources
-  "Get all of the sources. Returns a list of maps constructed from the
-returned SourceDTO objects."
-  [version]
-  getAllSources (map bean))
+  ;;; Term type metadata
+  getTermType [version term-type] [bean]
+  getAllTermTypes [version] [(map bean)]
 
+  ;;; Subheading metadata
+  getSubheading [version subheading] [bean]
+  getAllSubheadings [version] [(map bean)]
 
+  ;;; Relation metadata
+  getRelationLabel [version relation-label] [bean]
+  getAllRelationLabels [version] [(map bean)]
+  getAdditionalRelationLabel [version label] [bean]
+  getAllAdditionalRelationLabels [version] [(map bean)]
+  
+  ;;; Attribute metadata
+  getAttributeName [version attribute] [bean]
+  getAllAttributeNames [version] [(map bean)])
 
+;;; These two could be used to put the methods into a map if desired.
+;; (def ^:private metadata-methods
+;;   {"getSource" [["version" "source"] ["bean"]]
+;;    "getAllSources" [["version"] ["(map bean)"]]
+;;    })
 
-
-
-
-
-
-
+;; (defmacro def-fns-map []
+;;   `(do ~@(for [[meth [args transform-fn]] metadata-methods
+;;                :let [name (symbol (->kebab-case meth))]]
+;;            `(def ~name (gen-fn ~meth ~args ~transform-fn)))))
